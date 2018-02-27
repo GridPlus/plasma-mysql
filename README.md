5512b1e8d5a18bcd2d102aab42eaf28344d2ac13
Alex Miller <asmiller1989@gmail.com>
2018-02-27 14:01:58 -0800

    Adds basic docs and finishes API functions (first draft)

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # plasma-mysql
 mySQL wrapper for plasma functionality
+
+The Ethereum plasma [specification](https://ethresear.ch/t/minimal-viable-plasma/426) outlines a mechanism to deposit tokens of value into an auxiliary system and further guarantees users the ability to withdraw those tokens as long as they remain unspent.
+
+Although this side-system is presumed to be a blockchain (likely of centralized variety), there is no reason it cannot be a simple web service/API. This library outlines a minimum API for interacting with plasma tokens in mysql. It allows the operator to create UTXO 'tokens' to map to deposits on the root blockchain. It also allows users to spend those tokens (requiring a signature), and also accounts for withdrawals of unspent tokens.
+
+## Usage
+
+You can install with:
+
+```
+npm install plasma-mysql
+```
+
+The library is a wrapper over an existing `mysql` connection object:
+
+```
+var mysql = require('mysql');
+var PlasmaSql = require('plasma-sql').PlasmaSql;
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+// Create your plasma connection object
+var plasmaConn = new PlasmaSql(connection);
+
+// Record a deposit that happened on the root chain
+var deposit = {
+  to: '0x6ce07006399d7092513f708b0fbb2243a7822443',
+  value: 10,
+  txHash: '412831121b09286f71b74cbbc82e1f37a0592a75fab47f369464e074e1d9f934',
+}
+plasmaConn.recordDeposit(deposit, (err) => {
+  // deposit saved and utxo created (id = txhash)
+})
+
+// Do a general query
+plasmaConn.query(`SELECT * FROM Utxos WHERE id='${deposit.txHash}'`, (err, results, fields) => {
+  // same callback as mysql module
+})
+```
+
+## API
+
+plasma-sql provides an easy interface for managing plasma transactions off-chain.
+
+### self.recordDeposit(params, cb)
+
+Replay a deposit that was made on the root chain. This will create a UTXO object.
+
+### self.spendUtxo(params, cb)
+
+Spend a UTXO and create 1 or 2 new UTXOs from it. Signature from user is required.
+
+### self.startWithdrawal(params, cb)
+
+Replay the start of a withdrawal on the root chain. This will delete the corresponding UTXO in the database.
+
+## self.merge(params, cb)
+
+Merge two UTXOs for easier withdrawals on the root chain. Requires user signature. Destroys the original 2 UTXOs and creates a new one.
--- index.js
+++ index.js
@@ -21,17 +21,8 @@ class PlasmaSql {
 
     // First log the deposit
     const depositQ = logDeposit(to, value, txHash, chainId, tokenId);
-    this.query(depositQ, (err, results, fields) => {
-      if (err) { cb(err); }
-      else {
-        // Then create the UTXO
-        const utxoQ = createUtxo(to, value, txHash);
-        this.query(utxoQ, (err, results, fields) => {
-          if (err) { cb(err); }
-          else { cb(null, txHash); }
-        })
-      }
-    })
+    const utxoQ = createUtxo(to, value, txHash);
+    this.multiQuery([depositQ, utxoQ], cb);
   }
 
   // Allow a user to spend a UTXO and create 1 or 2 new UTXOs from that spend
@@ -49,12 +40,17 @@ class PlasmaSql {
         else if (utxo.value < value) { cb('Insufficient value in UTXO.'); }
         else {
           // Delete the original (old) UTXO
-          const delQ = utxo.delete(id);
-          this.query(delQ, (err) => {
+          this.deleteUtxos(id, (err) => {
             if (err) { cb(err); }
             else {
-              // Create 1 or 2 new UTXOs
-
+              // Create new UTXO ids deterministically
+              // Create the new UTXO(s)
+              const newId1 = this.newId(to, id);
+              const q1 = createUtxo(to, value, newId1);
+              const newId2 = value < utxo.value ? this.newId(signer, id) : null;
+              const q2 = newId2 == null ? null : createUtxo(to, utxo.value - value, newId2);
+              const qs = newId2 == null ? [q1] : [q1, q2];
+              this.multiQuery(qs, cb);
             }
           })
         }
@@ -74,6 +70,39 @@ class PlasmaSql {
   // Allow a user to merge two UTXOs
   merge(params, cb) {
     const { id1, id2, v, r, s } = params;
+    try {
+      const hash = sha3(`${id1}${id2}`);
+      const signerPubKey = ethutil.ecrecover(hash, v, r, s);
+      const signer = ethutil.publicToAddress(signerPubKey);
+      const q1 = utxo.getUtxo(id1);
+      const q2 = utxo.getUtxo(id2);
+      let sum = 0;
+      // Get the UTXOs and ensure the merger owns both
+      this.getOne(q1, (err, utxo1) => {
+        if (err) { cb(err); }
+        else if (utxo1.owner != signer) { cb('Tx1 does not belong to signer.'); }
+        else {
+          sum += utxo1.value;
+          this.getOne(q2, (err, utxo2) => {
+            if (err) { cb(err); }
+            else if (utxo2.owner != signer) { cb('Tx2 does not belong to signer.'); }
+            else {
+              sum += utxo2.value;
+              // Delete the original UTXOs
+              this.deleteUtxos([id1, id2], (err) => {
+                if (err) { cb(err); }
+                else {
+                  // Merge the UTXOs
+                  const newId = this.newId(signer, id1, id2);
+                  const createQ = createUtxo(signer, sum, newId);
+                  this.query(createQ, cb);
+                }
+              })
+            }
+          })
+        }
+      })
+    }
   }
 
   // Send a bunch of CREATE IF NOT EXISTS commands to the connection.
@@ -98,6 +127,38 @@ class PlasmaSql {
 
   quit() {
     this.conn.end();
+  }
+
+  // Delete one or more utxos. Expecting ids to be an array, but can handle
+  // a single number
+  deleteUtxos(ids, cb, outerCb=null) {
+    if (typeof ids == 'number') { ids = [ids]; }
+    if (outerCb == null) { outerCb = cb; }
+    if (ids.length == 0 ) { outerCb(null); }
+    else {
+      const id = ids.pop();
+      const delQ = utxo.delete(id);
+      this.query(delQ, (err) => {
+        if (err) { outerCb(err); }
+        else { deleteUtxos(ids, cb, outerCb); }
+      })
+    }
+  }
+
+  // Play multiple queries in sequence. The order is preserved.
+  multiQuery(queries, cb, outerCb=null) {
+    if (queries.length == 0) { outerCb(null); }
+    else {
+      const q = queries.reverse().pop();
+      this.query(q, (err) => {
+        if (err) { outerCb(err); }
+        else { multiQuery(queries, cb, outerCb); }
+      })
+    }
+  }
+
+  newId(to, oldId, extraData=1) {
+    return sha3(to, oldId, extraData);
   }
 
 }
